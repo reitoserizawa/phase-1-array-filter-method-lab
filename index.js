d0173690838075c3886344ab61f5662046642ee3
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-07 10:05:33 -0400

    commit tree 7fa9791e8bf781ed58c148e1facbc3b4f38904fa

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution here
+// // Code your solution here
+
+function findMatching (arrayDriversNames, strings) {
+    let matchingList = arrayDriversNames.filter (arrayDriversNames => arrayDriversNames.toLowerCase() === strings.toLowerCase())
+    return matchingList
+}
+
+function fuzzyMatch (arrayDriversNames, string) {
+    return arrayDriversNames.filter (whatever => whatever.substring(0, string.length) === string)
+}
+
+function matchName (arrayDriversNames, string) {
+    return arrayDriversNames.filter (heyheyhey => heyheyhey.name === string)  
+}
+
+// matchName - This function takes an array of driver objects and a string as arguments. Each driver object has two properties:
+//  name and hometown.
+//  The function should return each element whose name property matches the provided string argument.
+
+
+
+
+
+
+// /*
+// Use filter() to Return Matching Results
+// We have an array of drivers with various information. 
+// We need to write functions using the filter() method 
+// so that PickMeUp Taxi service employees can easily query the data. 
+// Run the tests to see 
+// what conditions need to be met by each function before you start writing JavaScript code.
+
+// You'll be writing three functions:
+
+// findMatching- This function takes an array of drivers' names and a string as arguments, 
+// and returns the matching list of drivers. The function should be case insensitive.
+
+
+// fuzzyMatch - This function takes an array of drivers' names 
+// and a string as arguments for querying the array, 
+
+// and returns all drivers whose names begin with the provided letters.
+
+// */
+
+
